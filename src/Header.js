19ac92aaa7ac5fea97715b9547fdb9ef2592718b
sumit <sumitkhobragade1010@gmail.com>
2022-06-29 23:53:52 +0530

    navbar completed products added

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 function Header() {
   return (
     <div className="header">
@@ -10,7 +12,7 @@ function Header() {
         alt=""
       />
       <div className="header__search">
-        <input className="header__searchIn" type="text" />
+        <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
@@ -25,6 +27,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
